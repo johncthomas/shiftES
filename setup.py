338da9bb337646697a47ba00cd6a95bd730b5c5d
johnc.thomas <jct61@cam.ac.uk>
2022-10-11 14:17:49 +0100

    pkg_resources not a requirement

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author = 'John C. Thomas',
     author_email = 'jcthomas000@gmail.com',
     version='0.1',
-    install_requires = ['numpy', 'pandas', 'pkg_resources'],
+    install_requires = ['numpy', 'pandas'],
     scripts = ['shiftes/shiftes.py'],
     python_requires = '>=3.7', #
     include_package_data=True, #uses MANIFEST.in
