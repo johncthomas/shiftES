2e86bd484648de15eee2f4644404be02fce24539
johnc.thomas <jct61@cam.ac.uk>
2022-10-13 10:34:22 +0100

    script path

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author_email = 'jcthomas000@gmail.com',
     version='0.1',
     install_requires = ['numpy', 'pandas'],
-    scripts = ['shiftes/calculate_effectsize.py'],
+    scripts = ['shiftES/calculate_effectsize.py'],
     python_requires = '>=3.6',
     include_package_data=True, #uses MANIFEST.in
 )
