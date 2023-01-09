720b6fe4c16bc180932e27dc6a51082d78406ab7
johnc.thomas <jct61@cam.ac.uk>
2023-01-09 14:17:36 +0000

    version bump because the readme V1.0.6 on pypi contains a typo

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ setup(
     description='Implimentation of "A Robust Nonparametric Measure of Effect Size Based on an Analog of Cohens d...", '
                 'R. Wilcox (2018). https://dx.doi.org/10.22237/jmasm/1551905677',
     author_email = 'jcthomas000@gmail.com',
-    version='1.0.5',
+    version='1.0.7',
     install_requires = ['numpy', 'pandas', 'openpyxl'],
     readme = "README.md",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type='text/markdown', # required so pypi renders it right
     scripts = ['shiftES/shift_effectsize.py'],
     python_requires = '>=3.6',
     include_package_data=True, #uses MANIFEST.in
