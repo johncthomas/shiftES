26a2720c95fd0bd3066a02bdc66295304266b746
johnc.thomas <jct61@cam.ac.uk>
2023-01-09 11:25:10 +0000

    whitespace

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name = 'shiftES',
     author = 'John C. Thomas',
-    description='Implimentation of "A Robust Nonparametric Measure of Effect Size Based on an Analog of Cohens d...", R. Wilcox (2018). https://dx.doi.org/10.22237/jmasm/1551905677',
+    description='Implimentation of "A Robust Nonparametric Measure of Effect Size Based on an Analog of Cohens d...", '
+                'R. Wilcox (2018). https://dx.doi.org/10.22237/jmasm/1551905677',
     author_email = 'jcthomas000@gmail.com',
     version='1.0.5',
     install_requires = ['numpy', 'pandas', 'openpyxl'],
